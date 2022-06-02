beefbe9675da3a36086b1b434db0b5e174109e4a
beetosu <cpwalsh@buffalo.edu>
2022-06-02 19:17:55 -0400

    template updates

--- server.py
+++ server.py
@@ -4,6 +4,11 @@ import books
 bookDict = books.create()
 app = Flask(__name__)
 
+@app.template_filter()
+def format_percentage(value, outOf):
+    percent = (value / outOf) * 100
+    return f"{round(percent, 1)}%"
+
 @app.route("/book/<bookId>")
 def hello_world(bookId):
     return render_template('book.html', book=bookDict[bookId])
