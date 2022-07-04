83441023ba69d8cac369fb70ce4b6ae3203621b2
beetosu <cpwalsh@buffalo.edu>
2022-07-04 10:47:33 -0400

    Prevent author overflow

--- server.py
+++ server.py
@@ -10,6 +10,12 @@ def format_percentage(value, outOf):
     percent = (value / outOf) * 100
     return f"{round(percent, 1)}%"
 
+@app.template_filter()
+def format_author(author, authorLst):
+    if len(', '.join(authorLst)) > 32:
+        return author.split(" ")[1]
+    return author
+
 @app.route("/")
 def homepage():
     return render_template('homepage.html', book=bookDict[bookLatest])
